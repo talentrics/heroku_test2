a2c79218e387502490f77c120026a6b3c7f9bde2
talentrics <daniel@talentrics.com>
2020-09-17 21:01:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
-from flask import Flask, render_template, request, redirect
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-  return render_template('index.html')
-
-@app.route('/about')
-def about():
-  return render_template('about.html')
-
-if __name__ == '__main__':
-  app.run(port=33507)
+def hello_world():
+    return 'Hello, World!'
